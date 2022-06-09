2c27d3978ee493cb1daaf50f91a473329f6d1f4f
Mertakbas <mertakbas@saatteknoloji.com>
2022-06-09 22:18:20 +0300

    Avoiding Conditionals in render

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Spinner from './Spinner';
 
 
 class App extends React.Component {
-    state = {lat: null, errorMessage: ''};
+    state = { lat: null, errorMessage: '' };
 
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
@@ -14,20 +14,25 @@ class App extends React.Component {
         );
     }
 
-
-
-    // React says we have to define render!!
-    render() {
-        if(this.state.errorMessage && !this.state.lat){
+    renderContent() {
+        if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
-        else if(this.state.lat && !this.state.errorMessage){
+        else if (this.state.lat && !this.state.errorMessage) {
             return <SeasonDisplay lat={this.state.lat} />;
         }
         else {
-            return <Spinner  message="Pleace accept location request"/>;
+            return <Spinner message="Pleace accept location request" />;
         }
     }
+
+    render() {
+        return (
+            <div className='border red'>
+                {this.renderContent()}
+            </div>
+        );
+    };
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
