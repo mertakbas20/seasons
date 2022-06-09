9ff3fa3e558209d7f4c9fc97f9a38a6fc139e5d4
Mertakbas <mertakbas@saatteknoloji.com>
2022-06-09 13:42:47 +0300

    ifelseifelse

--- src/index.js
+++ src/index.js
@@ -4,28 +4,46 @@ import ReactDOM from 'react-dom';
 
 
 class App extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        
+
         //THIS IS THE ONLY TIME we direct assignment
         //to this.state
-        this.state = {lat: 40};
+        this.state = { lat: null, errorMessage: '' };
 
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 // we called setState!!!!
-                this.setState({ lat: position.coords.latitude});
+                this.setState({ lat: position.coords.latitude });
 
                 //we did not!!
                 // this.state.lat= position.coords.latitude %böyle direk atama yapılmamalı
-            } ,
-            err => console.log(err)
+            },
+
+            err => {
+                this.setState({ errorMessage: err.message });
+            }
         );
     }
     // React says we have to define render!!
     render() {
-       
-        return <div>Latitude: {this.state.lat}</div>;
+        if(this.state.errorMessage && !this.state.lat){
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+        else if(this.state.lat && !this.state.errorMessage){
+            return <div>Latitude: {this.state.lat}</div>
+        }
+        else {
+            return <div>Loading...</div>
+        }
+
+        // return (
+        //     <div>
+        //         Latitude: {this.state.lat}
+        //         <br />
+        //         Error:{this.state.errorMessage}
+        //     </div>
+        // );
     }
 }
 
